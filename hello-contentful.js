40d0fb9313f9631f18f2d03df06765501abfa7e2
mccannsean421 <mccannsean421@gmail.com>
2017-06-28 22:25:55 +0100

    Retrieved blog posts

--- hello-contentful.js
+++ hello-contentful.js
@@ -2,13 +2,37 @@ var contentful = require('contentful')
 var util = require('util')
 var client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: 'developer_bookshelf',
+  space: 'nckonzuwzubf',
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: '0b7f6x59a0'
+  accessToken: '08c680e8b88f4b231dc8393a2332cd1aa217266db64554d95194937ce32017f5'
 });
 
 // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
-client.getEntry('5PeGS2SoZGSa4GuiQsigQu')
+client.getContentType('2wKn6yEnZewu2SCCkus4as')
+.then(function (contentType) {
+  //console.log(util.inspect(contentType, {depth: null}))
+})
+
+//return a single a entry
+client.getEntry('4KgYnruiYUeCs6WK4Uw6uc')
 .then(function (entry) {
-  console.log(util.inspect(entry, {depth: null}))
+  // logs the entry metadata
+  //console.log(entry.sys)
+
+  // logs the field with ID title
+  //console.log(entry.fields.title)
+})
+
+//Get ALL ENTRIES!!!!!
+client.getEntries()
+.then(function (entries) {
+  // log the title for all the entries that have it
+  entries.items.forEach(function (entry) {
+    if(entry.fields.title) {
+      console.log(entry.fields.title)
+    }
+    if(entry.fields.body) {
+    	console.log(entry.fields.body)
+    }
+  })
 })
