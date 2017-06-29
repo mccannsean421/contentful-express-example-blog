cd22b5619af3ccfe0588e2a5ac84ebec1ab4c199
mccannsean421 <mccannsean421@gmail.com>
2017-06-29 07:53:08 +0100

    Updated routing

--- app.js
+++ app.js
@@ -15,38 +15,9 @@ var helmet = require('helmet')
 
 var app = express()
 
-/**** Services ****/
-var contentful = require('contentful')
-var util = require('util')
-
-//Credentials
-var client = contentful.createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: '',
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: ''
-});
-
-var blogs = [];
-
-//Get ALL ENTRIES!!!!!
-client.getEntries()
-.then(function (entries) {
-  // log the title for all the entries that have it
-  entries.items.forEach(function (entry) {
-    if(entry.fields.title) {
-      var blogTitle = entry.fields.title;
-      console.log(entry.fields.title);
-      blogs.push(blogTitle);
-    }
-  })
-})
-
 /****** ROUTING ******/
-app.get('/', function (req, res) {
-  //res.send('hello world')
-  res.send(blogs[0]);
-})
+var index = require('./routes/index.js') //define route
+app.use('/', index) //apply route
 
 /***** PORT CONFIGURATION *****/
 app.listen(3000, function () {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express')
-var router = express.Router() //Needed for route files
-var app = express()
+var router = express.Router()
 
-// respond with "hello world" when a GET request is made to the homepage
+router.get('/', function (req, res, next) {
+  res.send('Testing');
+})
+
+module.exports = router
