34bcd89f877c4ecf4315d979547c2f617bd775ad
mccannsean421 <mccannsean421@gmail.com>
2017-07-03 07:54:58 +0100

    Pulled and displayed all entries on index

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,36 @@ var router = express.Router()
 var index = require('../services/index')//require corresponding service
 var client = require('../services/contentfulClient').client
 
+var exampleBlogs = [];
 
+/*client.getEntry('4KgYnruiYUeCs6WK4Uw6uc')
+.then(function (entry) {
+  example = entry.fields.title;
+  exampleBody = entry.fields.body;
+  //console.log(example);
+})*/	
+
+client.getEntries()
+.then(function (entries) {
+  // log the title for all the entries that have it
+  entries.items.forEach(function (entry) {
+    if(entry.fields.title) {
+      //console.log(entry.fields.title)
+      //console.log(entry.fields.body)
+      var bleg = {
+      	title: entry.fields.title,
+      	body: entry.fields.body
+      }
+      exampleBlogs.push(bleg)
+      console.log(exampleBlogs);
+    }
+  })
+})
 
 
 router.get('/', function (req, res, next) {
-  //res.render('./../views/index.jade');
-  	var blogs = []
-
-	client.getEntries()
-	.then(function (entries) {
-	  entries.items.forEach(function (entry) {
-	    if(entry.fields.title) {
-	      //console.log(entry.fields.title);
-	      blogs.push(entry.fields.title)
-	      console.log(blogs);
-	    }
-	  })
-	})
-	
-
 	res.render('./../views/index.jade', {
-		'blogs': ['blog 1', 'blog 2']
+		'blogs': exampleBlogs,
 	});
 })
 
