25e4ad2536e65fbe7c5c694dc7a9a13a2522c546
mccannsean421 <mccannsean421@gmail.com>
2017-07-08 12:44:49 +0100

    Retrieved author content info, added profile picture from contentful

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,7 @@ client.getEntries()
         blogPath: entry.fields.title.replace(/\s+/g, '-').toLowerCase() 
       }
       exampleBlogs.push(bleg)
-      //bleg.title.replace(/\s+/g, '-').toLowerCase()) //adjust strings and add to paths
-      console.log(bleg.blogPath);
+      //console.log(bleg.blogPath);
       router.get('/' + bleg.blogPath, function (req, res) {
         res.render('./../views/blogPost.jade', {
           'title': bleg.title,
@@ -32,26 +31,33 @@ client.getEntries()
   })
 })
 
-
+/**** INDEX ****/
 router.get('/', function (req, res, next) {
 	res.render('./../views/index.jade', {
 		'blogs': exampleBlogs,
 	});
 })
 
+/**** ABOUT ****/
 router.get('/about', function (req, res) {
-  res.render('./../views/about.jade');
+  client.getEntry('5wcUDTrUoEowEAA6WsG2iM')
+    .then(function (entry) {
+      
+      //retrieve photo
+      var author = {
+        authorName: entry.fields.name
+      }
+      res.render('./../views/about.jade', {
+        'authorName': author.authorName,
+        'authorProfilePicSrc': 'https://images.contentful.com/nckonzuwzubf/6lwsOcpyCc8sIC2MqMCEQC/f7769c5011027591f92688cdba6eba63/Snapchat-6200244860743713401.jpg'
+      });
+  })
+
 })
 
 router.get('/blog-archive', function (req, res) {
   res.render('./../views/blogs.jade');
 })
-
-/*router.get('/blog-post', function (req, res) {
-  res.render('./../views/blogPost.jade', {
-    'currentBlog': 'Bro'
-  });
-})*/
 
 //Generate router views
 for(var i = 0; i < blogPaths.length; i++) {
